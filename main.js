47b7dd2f4d0e470283621d994f7a071f87a0c96a
pixeldude41 <47810442+pixeldude41@users.noreply.github.com>
2019-05-10 17:05:11 +0100

    changed

--- main.js
+++ main.js
@@ -2,16 +2,8 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = __importDefault(require("discord.js"));
-const ConfigFile = __importStar(require("./src/config"));
 require('dotenv').config();
 var client = new discord_js_1.default.Client();
 let commands = [];
@@ -24,6 +16,22 @@ client.on("guildMemberAdd", member => {
     welcomeChannel.send(`Welcome ${member.displayName}! We hope you enjoy your time here!`);
     member.send("Thank you for Joining our server!");
     member.send("Hello, My name is EXL! As you know I'm just a regular bot and anyways if you need help with anything in the near future just type help in chat we will be right their to help you out gladly! Best Wishes EXL ");
+    client.on('message', message => {
+        let args = message.content.substring(PREFIX.length).split(" ");
+        switch (args[0]) {
+            case 'epic':
+                const embed = new discord_js_1.default.RichEmbed()
+                    .setTitle('User Information')
+                    .addField('Player Name', message.author.username, true)
+                    .addField('Version', version, true)
+                    .addField('Current Server', message.guild.name, true)
+                    .setColor(0xF1C40F)
+                    .setThumbnail(message.author.avatarURL)
+                    .setFooter('As you know I am 24/7!');
+                message.channel.sendEmbed(embed);
+                break;
+        }
+    });
 });
 client.on("guildMemberRemove", member => {
     let welcomeChannel = member.guild.channels.find(channel => channel.name === "welcome");
@@ -53,4 +61,3 @@ client.on('message', message => {
     }
 });
 client.login(process.env.DISCORD_TOKEN);
-client.login(ConfigFile.config.token);
