3be6f3647b6092030eafedb142959351324dd6b9
pixeldude41 <47810442+pixeldude41@users.noreply.github.com>
2019-05-12 14:02:36 +0100

    kkk

--- main.ts
+++ main.ts
@@ -99,32 +99,7 @@ client.on(`message`, async message => {
     }
 })
     
-client.on(`message`, message => {
-    let msg = message.content.toUpperCase();
-    let sender = message.author;
-    let cont = message.content.slice(PREFIX.length).split(" ");
-    let args = cont.slice(1);
-    
-    if(msg.startsWith(PREFIX + 'PURGE')) {
-        async function purge() {
-            message.delete();
 
-            if (!message.member.roles.find("name", "GOD")) {
-                message.channel.send('You need the \`GOD\` role to use this command.');
-                return;
-            }
-            if (isNaN(arguments[0])) {
-                message.channel.send('Please use a number as your arguements. \n Usage: ' + PREFIX + 'purge <amount>');
-                return;
-            }
-            const fetched = await message.channel.fetchMessages({limit: arguments[0]});
-            console.log(fetched.size + ' messages found, deleting...');
-            message.channel.bulkDelete(fetched)
-            .catch(error => message.channel.send(`Error: ${error}`));
-        }
-
-    }
-});
 client.on ("message", (message) => {
     let args = message.content.substring(PREFIX.length).split(" ");
 
